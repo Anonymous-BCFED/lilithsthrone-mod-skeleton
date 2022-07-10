3f2180b201707fa2183c153445e11295975c7c3d
Anonymous-BCFED <nope@no.pe>
2022-07-09 19:04:14 -0700

    Update Skellington

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ This is a long list, but you only have to do it once.
 1. Edit `build.sh` and `build.bat`.
 1. Replace `LICENSE` with whatever license you want.  
     * You should use an open-source license like MIT, BSD, GPLv2, GPLv3, etc.  [This website](https://choosealicense.com/licenses/) can help you choose.  Keep in mind that people may want to *fork* (make their own clone of) your project if you stop maintaining it at some point.
-    * While *this repo* is licensed MIT, you are given special permission to relicense the contents and remove credits.
+    * While *this repo* is licensed MIT, you are given special permission to relicense the contents and remove credits so you can make your own project.
 1. `git add .` to stage all your files for the commit we're about to make.
 1. `git commit -a -m "Initial commit"` to commit all your changes to your local copy of your repository.
 1. Open project with your favorite editor and start editing!
--- src/plugin/myplugin/MyExamplePlugin.java
+++ src/plugin/myplugin/MyExamplePlugin.java
@@ -3,11 +3,17 @@
  */
 package plugin.myplugin;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import com.lilithsthrone.game.character.fetishes.Fetish;
+import com.lilithsthrone.game.character.GameCharacter;
+import com.lilithsthrone.game.character.effects.AbstractPerk;
+import com.lilithsthrone.game.character.fetishes.AbstractFetish;
 import com.lilithsthrone.game.character.npc.NPC;
+import com.lilithsthrone.game.inventory.enchanting.PossibleItemEffect;
 import com.lilithsthrone.modding.BasePlugin;
 
 /**
@@ -19,9 +25,9 @@ public class MyExamplePlugin extends BasePlugin {
 	@Override
 	public void onStartup() {
 		// This is called when your plugin is first loaded.
-		//System.out.println("Hello world!");
+		// System.out.println("Hello world!");
 	}
-	
+
 	// You can get rid of any of these you don't use.
 
 	@Override
@@ -30,27 +36,28 @@ public class MyExamplePlugin extends BasePlugin {
 		// The super returns true if all the tags are satisfied
 		return super.isSatisfied(loadedTags);
 	}
-	
+
 	@Override
-	public void addFetishes(HashSet<Fetish> providedFetishes) {
-		// Add new fetishes here.
-		// Only called once.
-		//providedFetishes.add(new MyWeirdFetish());
+	public void onNPCGenerateTransformativePotion(NPC npc, GameCharacter target,
+			List<PossibleItemEffect> possibleEffects) {
+		// TODO Auto-generated method stub
+		super.onNPCGenerateTransformativePotion(npc, target, possibleEffects);
 	}
 
 	// Called after perks are all loaded.
 	@Override
 	public void onInitPerks() {
-		//Perk.addPerk("PERK_ID", new MyCoolPerk());
+		// Perk.addPerk("PERK_ID", new MyCoolPerk());
 	}
 
 	@Override
 	public void onInitUniqueNPCs(List<Class<? extends NPC>> addedNpcs) {
 		// Add custom NPCs here.
-		//if(!Main.game.NPCMap.containsKey(Main.game.getUniqueNPCId(HeadlessHorseman.class))) { 
-		//	addNPC(new GaryBuseyNPC(), false); 
-		//	addedNpcs.add(GaryBuseyNPC.class); 
-		//}
+		// if(!Main.game.NPCMap.containsKey(Main.game.getUniqueNPCId(HeadlessHorseman.class)))
+		// {
+		// addNPC(new GaryBuseyNPC(), false);
+		// addedNpcs.add(GaryBuseyNPC.class);
+		// }
 	}
 
 	@Override
@@ -66,6 +73,38 @@ public class MyExamplePlugin extends BasePlugin {
 	@Override
 	public void onPluginsLoaded() {
 		// Called after all plugins are loaded.
+	}
+
+	@Override
+	public void addPerks(Set<AbstractPerk> providedPerks) {
+		// Add your own perks here.
+	}
+
+	@Override
+	public Collection<? extends String> getProvidedTags() {
+		// TODO Auto-generated method stub
+		return super.getProvidedTags();
+	}
+
+	@Override
+	public void getRelatedFetishes(List<AbstractFetish> fetishes, GameCharacter characterPerforming,
+			GameCharacter characterTargeted, boolean isPenetration, boolean isOrgasm) {
+		// TODO Auto-generated method stub
+		super.getRelatedFetishes(fetishes, characterPerforming, characterTargeted, isPenetration, isOrgasm);
+	}
+
+	@Override
+	public void onAfterGenerateDesires(GameCharacter character, List<AbstractFetish> availableFetishes,
+			Map<AbstractFetish, Integer> desireMap, Map<AbstractFetish, Integer> negativeMap, int desiresAssigned) {
+		// TODO Auto-generated method stub
+		super.onAfterGenerateDesires(character, availableFetishes, desireMap, negativeMap, desiresAssigned);
+	}
+
+	@Override
+	public void onGenerateDesiresAvailableFetishesFixup(GameCharacter character,
+			List<AbstractFetish> availableFetishes) {
+		// TODO Auto-generated method stub
+		super.onGenerateDesiresAvailableFetishesFixup(character, availableFetishes);
 	}
 
 }
